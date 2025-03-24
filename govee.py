bcf630f843f9887bc6d6becb7cca92347ffe7c46
Mia Neil <mia.e.atkinson@gmail.com>
2025-03-24 15:38:09 -0400

    NCAA functioning, adding govee light interaction

--- govee.py
+++ govee.py
@@ -1,32 +1,80 @@
 import requests
-from bs4 import BeautifulSoup
-import sys
-import logging
-import boto3
 import urllib.request
 import json
-import os
 from pprint import pformat
 import pprint
-import time
 
-logging.getLogger().setLevel(logging.INFO)
 
 URL = 'https://openapi.api.govee.com'
-	# (f"get_roster_unique Exception Raised: {e}")
 
 def main():
-	my_devices = get_devices()
+	f = open("api_key", "r")
+	key = f.read().replace('\n', '')
+
+	my_devices = get_devices(key)
+
 	for item in my_devices:
 		if item['type'] == 'devices.types.light':
-			# power_toggle(item['sku'], item['device'], 1)
-			color_function(item['sku'], item['device'], 'blue')
+			print(item['deviceName'])
+			# get_state(item, key)
+			get_dynamic_scene(item, key)
 
-def power_toggle(sku, device, command):
-	print("power toggle")
+def get_dynamic_scene(device, key):
+	url = '/router/api/v1/device/scenes'
+	payload = {
+		"requestId": "uuid",
+		"payload": {
+			"sku": device['sku'],
+			"device": device['device']
+		}
+	}
+	headers = {'Govee-API-Key': key, 'Content-Type': 'application/json'}
+	try:
+		response = requests.post(URL+url, json=payload, headers=headers)
+		json_response = json.loads(response.text)
+		with open('govee_output.json', 'w', encoding='utf-8') as f:
+			json.dump(json_response, f, ensure_ascii=False, indent=4)
+		return(json_response)
+	except Exception as e:
+		raise(e)
 
-def color_function(item['sku'], item['device'], color):
-	print('set color')
+def get_state(device, key):
+	url = '/router/api/v1/device/state'
+	payload = {
+		"requestId": "uuid",
+		"payload": {
+			"sku": device['sku'],
+			"device": device['device']
+		}
+	}
+	headers = {'Govee-API-Key': key, 'Content-Type': 'application/json'}
+	try:
+		response = requests.post(URL+url, json=payload, headers=headers)
+		json_response = json.loads(response.text)
+		with open('govee_output.json', 'w', encoding='utf-8') as f:
+			json.dump(json_response, f, ensure_ascii=False, indent=4)
+		return(json_response)
+	except Exception as e:
+		raise(e)
+
+
+def get_devices(key):
+	url = '/router/api/v1/user/devices'
+	headers = {'Govee-API-Key': key, 'Content-Type': 'application/json'}
+
+	try:
+		response = requests.get(URL+url, headers=headers)
+		json_response = json.loads(response.text)
+		return(json_response['data'])
+		# for product in json_response['data']:
+		# 	print(product['deviceName'])
+
+		# with open('govee_output.json', 'w', encoding='utf-8') as f:
+		# 	json.dump(json_response, f, ensure_ascii=False, indent=4)
+
+	except Exception as e:
+		raise(e)
+
 
 if __name__=="__main__":
-    main()
+	main()
--- local.sh
+++ local.sh
@@ -5,7 +5,8 @@ export PATH=/usr/local/bin:$PATH
 source ~/.bash_profile
 
 clear
-python3 game_info.py
+# python3 game_info.py
+python3 govee.py
 # SCHOOL_NAME=$1
 # TEAM=$2
 # X={'"'School'"':'"'$SCHOOL_NAME'"','"'Team'"':'"'$TEAM'"'}
